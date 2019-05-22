dc53afb05ce083f000e8fef29a5ad498ab2fee33
Martnick1 <matthewmartnick@gmail.com>
2019-05-22 00:29:15 -0400

    Began refactor

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
+    var currentScreen = document.getElementById("character-select-screen");
+
     function Character(first, last, health, attack, image) {
         this.firstName = first;
         this.lastName = last;
@@ -11,44 +13,52 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Create character objects
     var cloud = new Character("Cloud", "Strife", 120, 16, "../images/cloudSml.png");
-
+    
     var Character = cloud;
 
-    var charDiv = document.createElement("div").setAttribute("class", "character") /*.setAttribute("data-name", Character.firstName)*/ ;
-    //var charImage =  document.createElement("IMG").setAttribute("alt", "image").setAttribute("class", "character-image").setAttribute("src", Character.imageURL);
+    var renderCharacter = function (Character, renderArea) {
 
-    //function imgPop() {
+        var charDiv = document.createElement("DIV");
+        charDiv.setAttribute("class", "character");
+        charDiv.setAttribute("data-name", Character.firstName);
+        renderArea.appendChild(charDiv);
 
-        var x = document.createElement("IMG");
-        x.setAttribute("src", Character.imageUrl.toString());
-        x.setAttribute("class", "character-image character");
-        x.setAttribute("alt", "image");
-        document.getElementById("character-select-screen").appendChild(x);
-    //};
+        var charName = document.createElement("P");
+        charName.innerText = Character.firstName;
+        charName.setAttribute("class", "character-name");
+        charDiv.appendChild(charName);
 
-    charDiv;
+        var charImage = document.createElement("IMG");
+        charImage.setAttribute("src", Character.imageUrl.toString());
+        charImage.setAttribute("class", "character-image");
+        charImage.setAttribute("alt", "image");
+        charDiv.appendChild(charImage);
+
+        renderArea.appendChild(charDiv);
+    };
+
+    renderCharacter(Character, currentScreen);
     
-    document.createElement("div").setAttribute("class", "character");
-    //document.getElementById("team-character-select").innerHTML = Character.imageUrl;
-    //document.getElementById("character-select-screen").innerHTML = imgPop();
-    //document.getElementById("team-character-select").innerHTML = charDiv+ " " + charImage+ "."; 
-
-
-
-
-    // renders all characters to the DOM
-    // function renderCharacter(character, renderArea) {
-
-    // this block of code builds the character card, and renders it to the page.
-
-
-
-    //var charHealth = $("<div class='character-health'>").text(character.health);
-    //charDiv.appendChild(charImage); //.append(charHealth);
-    //};
-
-
-    //renderCharacter(Character, document.getElementById("team-character-select"));
-
-
 });
+
+//document.createElement("DIV").createAttribute("class", "new");
+//document.getElementById("team-character-select").innerHTML = Character.imageUrl;
+//document.getElementById("character-select-screen").innerHTML = imgPop();
+//document.getElementById("team-character-select").innerHTML = charDiv+ " " + charImage+ "."; 
+
+
+
+
+// renders all characters to the DOM
+// function renderCharacter(character, renderArea) {
+
+// this block of code builds the character card, and renders it to the page.
+
+
+
+//var charHealth = $("<div class='character-health'>").text(character.health);
+//charDiv.appendChild(charImage); //.append(charHealth);
+//};
+
+
+//renderCharacter(Character, document.getElementById("team-character-select"));
