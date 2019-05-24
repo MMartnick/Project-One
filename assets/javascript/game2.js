dd1b7c7f5e64014b6d4dd5b4344afc41d353af0c
Martnick1 <matthewmartnick@gmail.com>
2019-05-23 22:25:29 -0400

    finished refactoring the render character functions

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,7 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
+    /*/ Variables for audio ------------------------------------
+    var error = new Audio("assets/audio/error.mp3");
+    var intro = new Audio("assets/audio/intro.mp3");
+    var growl = new Audio("assets/audio/growl.mp3");
+    var cursor = new Audio("assets/audio/cursor.mp3");
+    var battle1 = new Audio("assets/audio/battle1.mp3");
+    //var boss1 = new Audio("assets/audio/boss1.mp3");
+    var fanfare = new Audio("assets/audio/fanfare.mp3");
+    var enemyDeath = new Audio("assets/audio/enemyDeath.mp3"); */
+
     var currentScreen = document.getElementById("team-character-select");
+
+    // Global variables
+    var dude;
+    var charCounter = 2;
+    var killCount = 0;
+    var enemySelected;
+    var combatToggle;
+    var selectToggle;
+    var startToggle;
+    var currentEnemy;
+    var temperature;
+    var charHealth;
+    //var battleMusic = battle1;
 
 
     // Character function =========================================================
@@ -14,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 
-    
+
     // Create character objects
     var cloud = new Character("Cloud", "Strife", 120, 16, "../images/cloudSml.png");
     var x = cloud;
@@ -22,9 +45,9 @@ document.addEventListener("DOMContentLoaded", function () {
     var y = golbez;
     var lulu = new Character("Lulu", null, 140, 15, "../images/luluSml.png");
     var z = lulu;
-    
+
     // Array of characters
-    var charList = [x, y, z ]; 
+    var charList = [x, y, z];
 
     //==============================================================================
 
@@ -59,4 +82,59 @@ document.addEventListener("DOMContentLoaded", function () {
         renderCharacter(charList[i], currentScreen);
     }
     //==============================================================================
+
+    var charObj;
+    document.getElementsByClassName('character').onclick = function updateCharacter() {
+
+
+
+        if (currentScreen = document.getElementById("team-character-select")) {
+            charObj = document.getElementsByClassName('character');
+            updateCharacter();
+            console.log(charObj);
+            selectedSection = document.getElementById("selected-characters");
+            renderCharacter(charObj, selectedSection);
+
+        }
+    };
+    selectedSection = document.getElementById("selected-characters");
+    renderCharacter(cloud, selectedSection);
+    cloud.onclick = function () {
+        console.log("yo");
+    }
+
 });
+
+
+
+
+
+/*
+
+// function for selecting characters
+$("#team-character-select").on("click", ".character", function () {
+    // Saving the clicked character's name.
+    dude = $(this).attr("data-name");
+    console.log(dude);
+
+    if (charCounter > 0) {
+        charCounter = charCounter - 1;
+        updateCharacter(characters[dude], "#selected-characters");
+        updateCharacter(characters[dude], "#player-section");
+        renderInfo(characters[dude], ".character-menu");
+        console.log(characters[dude]);
+        renderMoveSet(dude, ".moveset-container");
+        $(this).hide();
+        $("#char-select").show();
+        console.log("this is where I create a button" + characters[dude].name);
+        cursor.play();
+    } else {
+        error.play();
+    }
+});
+
+function updateCharacter(charObj, areaRender) {
+    // $(areaRender).empty();
+    console.log(charObj);
+    renderCharacter(charObj, areaRender);
+    */
